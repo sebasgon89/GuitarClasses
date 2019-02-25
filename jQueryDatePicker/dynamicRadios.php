77b726fbf0d94a0804c8069975fb445e03a542a7
SEBASTIAN GONZALEZ <sebasgon@ar.ibm.com>
2019-02-25 10:32:46 -0300

    More radio attributes

--- jQueryDatePicker/dynamicRadios.php
+++ jQueryDatePicker/dynamicRadios.php
@@ -13,19 +13,32 @@
     $('#boton').click(function(e){
          //e.preventDefaul<t();
          //$("#boton").after("<br><input type='radio'value='Appended item' class='boton'></input>");
-         $opt1 = "<br><input name='hour' type='radio'value='1400' class='boton'>14:00</input>";
-         $opt2 = "<br><input name='hour' type='radio'value='1500' class='boton'>15:00</input>";
-         $("#boton").after($opt1,$opt2);
+         $opt1 = "<br><label id='l1400'><input name='hour' id='h1400' type='radio'value='1400' class='boton'>14:00</input></label>";
+         $opt2 = "<br><label id='l1500'><input name='hour' id='h1500' type='radio'value='1500' class='boton'>15:00</input></label>";
+         $("#emptyP").after($opt1,$opt2);
+     });
+     $('#toDisable').click(function(e){
+        $("#h1400").prop('disabled', true);
+        $("#l1400").css('color', 'red');
      });
   });
 
 
   </script>
+
+  <style>
+    label {
+      font-weight: bold;
+    }
+  </style>
 </head>
 <body>
  
 <p>Available times: </p>
-<p><input type="button" value="Click me" name="boton" id="boton"> 
- 
+<p><input type="button" value="Click me" name="boton" id="boton">
+<p><input type="button" value="Disable first one" name="toDisable" id="toDisable"> 
+<p id="emptyP">
+
+
 </body>
 </html>
