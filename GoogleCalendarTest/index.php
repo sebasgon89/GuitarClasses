45f1fa97031ea268363e640379cdf163bb12b830
sebasgon89 <seba3789@gmail.com>
2019-03-09 18:09:59 -0300

    Added GoogleCalendar

---
+++ GoogleCalendarTest/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+        
+    </head>
+
+</head>
+<body style="background: white;">
+<iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=sebastiangonzalezg89%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FArgentina%2FBuenos_Aires" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</body>
+</html>
